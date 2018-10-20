8dd02554ca184308b7eb02148c70a548ef599492
suns <xfurickova@stuba.sk>
2018-10-20 22:35:24 +0200

    doplnenie

--- venv/bin/zadanie2.py
+++ venv/bin/zadanie2.py
@@ -9,7 +9,6 @@ import pickle
 import math
 import time
 
-
 def loadPickleFile(pathDirectory,val):
     try:
         if(val):
@@ -51,4 +50,21 @@ def euclideanDistance(fruit):
 
 fruit = loadPickleFile("/home/suns/Desktop/fruits-360/TrainData/Apple",True)
 
-euclideanDistance(fruit)
+
+distance = euclideanDistance(fruit)
+
+with open('testfile.txt', 'w') as f:
+    for item in distance:
+        f.write("%s\n" % item)
+
+# distance = [1,2,3,5,6,5,7,2,4,6]
+# n, bins, patches = plt.hist(x=d, bins='auto', color='#0504aa',
+#                             alpha=0.7, rwidth=0.85)
+# plt.grid(axis='y', alpha=0.75)
+# plt.xlabel('Value')
+# plt.ylabel('Frequency')
+# plt.title('My Very Own Histogram')
+# plt.text(23, 45, r'$\mu=15, b=3$')
+# maxfreq = distance.max()
+# # Set a clean upper y-axis limit.
+# plt.ylim(ymax=np.ceil(maxfreq / 10) * 10 if maxfreq % 10 else maxfreq + 10)
